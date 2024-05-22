4bfd3c5ac91b01fe1036f67b37adc54cc05bbcba
Uzi Refaeli <uzi.refaeli@healthee.co>
2024-05-22 22:43:05 +0300

    SIMD reads

---
+++ part3/perf_course/asm/listing_0150_read_widths.asm
@@ -0,0 +1,62 @@
+.text
+.global _Read_4x3
+.global _Read_8x3
+.global _Read_16x3
+.global _Read_32x3
+.global _Read_64x3
+.global _Read_128x3
+
+_Read_4x3:
+loop1:
+    ldr w2,[x1]
+    ldr w2,[x1, #4]
+    ldr w2,[x1, #8]
+    subs x0, x0, 12
+    bhi loop1
+    ret
+
+_Read_8x3:
+loop2:
+    ldr x2,[x1]
+    ldr x2,[x1, #8]
+    ldr x2,[x1, #16]
+    subs x0, x0, 24
+    bhi loop2
+    ret
+
+_Read_16x3:
+loop6:
+    LDR H0, [X1]
+    LDR H0, [X1, #16]
+    LDR H0, [X1, #32]
+    subs x0, x0, 48
+    bhi loop6
+    ret
+
+_Read_32x3:
+loop5:
+    LDR S0, [X1]
+    LDR S0, [X1, #32]
+    LDR S0, [X1, #64]
+    subs x0, x0, 96
+    bhi loop5
+    ret
+
+_Read_64x3:
+loop3:
+    LDR D0, [X1]
+    LDR D0, [X1, #64]
+    LDR D0, [X1, #128]
+    subs x0, x0, 192
+    bhi loop3
+    ret
+
+_Read_128x3:
+loop4:
+;    ld1 {V0.2D}, [x1]
+    ldr Q0, [x1]
+    ldr Q0, [x1, #128]
+    ldr Q0, [x1, #256]
+    subs x0, x0, 384
+    bhi loop4
+    ret
---
+++ part3/perf_course/src/bin/listing_0151_read_widths_main.rs
@@ -0,0 +1,109 @@
+extern crate perf_course;
+
+use std::cell::RefCell;
+use std::error::Error;
+use std::ptr;
+use std::rc::Rc;
+
+use libc::{mmap, size_t, MAP_ANON, MAP_PRIVATE, PROT_READ, PROT_WRITE};
+
+use perf_course::repetition_tester::repetition_tester::RepetitionTester;
+
+extern "C" {
+    fn Read_4x3(count: u64, data: *mut u8);
+    fn Read_8x3(count: u64, data: *mut u8);
+    fn Read_16x3(count: u64, data: *mut u8);
+    fn Read_32x3(count: u64, data: *mut u8);
+    fn Read_64x3(count: u64, data: *mut u8);
+    fn Read_128x3(count: u64, data: *mut u8);
+}
+
+struct TestParams {
+    // file_name: &'static str,
+    expected_bytes: u64,
+    seconds_to_try: u64,
+    buffer: *mut u8,
+}
+
+struct TesterFunction {
+    name: &'static str,
+    function: unsafe extern "C" fn(count: u64, data: *mut u8),
+    tester: Rc<RefCell<RepetitionTester>>,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let total_size = 1024 * 1024 * 1024;
+    let addr = unsafe {
+        mmap(
+            ptr::null_mut(), // Address at which to start the mapping (nullptr lets the OS choose)
+            total_size,       // Number of bytes to map
+            PROT_READ | PROT_WRITE, // Enable read and write access
+            MAP_PRIVATE | MAP_ANON,
+            -1, // File descriptor not used with MAP_ANON
+            0,  // Offset not used with MAP_ANON
+        )
+    };
+
+    let mut functions = vec![
+        TesterFunction {
+            name: "Read_4x3",
+            function: Read_4x3,
+            tester: Rc::new(RefCell::new(RepetitionTester::new())),
+        },
+
+        TesterFunction {
+            name: "Read_8x3",
+            function: Read_8x3,
+            tester: Rc::new(RefCell::new(RepetitionTester::new())),
+        },
+
+        TesterFunction {
+            name: "Read_16x3",
+            function: Read_16x3,
+            tester: Rc::new(RefCell::new(RepetitionTester::new())),
+        },
+
+        TesterFunction {
+            name: "Read_32x3",
+            function: Read_32x3,
+            tester: Rc::new(RefCell::new(RepetitionTester::new())),
+        },
+
+        TesterFunction {
+            name: "Read_64x3",
+            function: Read_64x3,
+            tester: Rc::new(RefCell::new(RepetitionTester::new())),
+        },
+
+        TesterFunction {
+            name: "Read_128x3",
+            function: Read_128x3,
+            tester: Rc::new(RefCell::new(RepetitionTester::new())),
+        },
+    ];
+
+    let params = TestParams {
+        expected_bytes: total_size as u64,
+        seconds_to_try: 2,
+        buffer: addr as *mut u8,
+    };
+
+    loop {
+        for ft in &functions {
+            println!("\n----- {} -----", ft.name);
+            let mut tester = ft.tester.borrow_mut();
+            tester.start_test_wave(params.seconds_to_try, params.expected_bytes);
+            let total_size = params.expected_bytes as size_t;
+            let buffer = unsafe { std::slice::from_raw_parts_mut(addr, total_size) };
+            while tester.is_testing() {
+                let mut i = 0;
+                tester.begin_time();
+                unsafe {
+                    (ft.function)(total_size as u64, buffer.as_mut_ptr() as *mut u8);
+                }
+                tester.end_time();
+                tester.count_bytes(total_size as u64);
+            }
+        }
+    }
+}
