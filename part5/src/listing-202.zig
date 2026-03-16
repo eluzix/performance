35e26471ac43d3837079a1434ac9b7a0e4142dc8
Uzi Refaeli <uzi.refaeli@healthee.co>
2026-03-16 22:53:44 +0200

    listing 202 'fix' the dependency chain

--- part5/src/listing-202.zig
+++ part5/src/listing-202.zig
@@ -31,25 +31,47 @@ fn fmaDepChain(chainCount: u32, chainLength: u32) void {
     }
 }
 
+fn fmaDepChainMy(chainCount: u32, chainLength: u32) void {
+    for (0..chainCount) |_| {
+        const X2: f64 = 0;
+        const M: f64 = 0;
+        var R0: f64 = 0;
+
+        mem.doNotOptimizeAway(X2);
+        mem.doNotOptimizeAway(M);
+        mem.doNotOptimizeAway(R0);
+
+        var lenIdx: u32 = 0;
+        while (lenIdx < chainLength) {
+            lenIdx += 8;
+
+            for (0..8) |_| {
+                R0 = @mulAdd(f64, R0, X2, M);
+            }
+        }
+
+        mem.doNotOptimizeAway(R0);
+    }
+}
+
 pub fn main() !void {
     fmaDepChain(2, 8);
 
-    var testSeries = repetitionTester.ReptitionTestSeries(1, 1024).new();
+    var testSeries = repetitionTester.ReptitionTestSeries(1024, 2).new();
     testSeries.setRowLabel(""[0..]);
 
     var chainLen: u32 = 8;
 
     while (chainLen <= 256) {
-        chainLen += 8;
-
         const repCount: u32 = 1024 * 1024;
         const chainCount: u32 = repCount / chainLen;
 
         var buf: [64]u8 = [_]u8{0} ** 64;
         _ = try std.fmt.bufPrint(&buf, "{d}", .{chainLen});
-        testSeries.setColumnLabel(buf[0..]);
-        var tester = repetitionTester.Tester.new();
+        testSeries.setRowLabel(buf[0..]);
 
+        testSeries.setColumnLabel("original");
+        var tester = repetitionTester.Tester.new();
         testSeries.newTestWave(&tester, 3, repCount);
         while (testSeries.isTesting(&tester)) {
             tester.beginTime();
@@ -57,6 +79,18 @@ pub fn main() !void {
             tester.endTime();
             tester.countBytes(repCount);
         }
+
+        testSeries.setColumnLabel("my");
+        tester = repetitionTester.Tester.new();
+        testSeries.newTestWave(&tester, 3, repCount);
+        while (testSeries.isTesting(&tester)) {
+            tester.beginTime();
+            std.mem.doNotOptimizeAway(fmaDepChainMy(chainCount, chainLen));
+            tester.endTime();
+            tester.countBytes(repCount);
+        }
+
+        chainLen += 8;
     }
     testSeries.dumpCSV(repetitionTester.Metrics.gbPerSec);
 }
